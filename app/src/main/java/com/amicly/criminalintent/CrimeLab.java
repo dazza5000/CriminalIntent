3091e8ed1142e6a5f6d14eadd13ce09ca91f43de
Darran Kelinske <Darran.Kelinske@servicemesh.com>
2016-02-18 11:00:03 -0600

    chapter_16 complete

--- app/src/main/java/com/amicly/criminalintent/CrimeLab.java
+++ app/src/main/java/com/amicly/criminalintent/CrimeLab.java
@@ -4,11 +4,13 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
 
 import com.amicly.criminalintent.database.CrimeBaseHelper;
 import com.amicly.criminalintent.database.CrimeCursorWrapper;
 import com.amicly.criminalintent.database.CrimeDbSchema.CrimeTable;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -71,6 +73,17 @@ public class CrimeLab {
             cursor.close();
         }
 
+    }
+
+    public File getPhotoFile(Crime crime){
+        File externalFilesDir = mContext
+                .getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        if (externalFilesDir == null){
+            return null;
+        }
+
+        return new File(externalFilesDir, crime.getPhotoFilename());
     }
 
     public void addCrime(Crime c){
